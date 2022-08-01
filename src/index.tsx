c2d06a2f0eb64cd847e084f60a0c47c6372bd2a6
William Edwards <shadowapex@gmail.com>
2022-08-01 12:45:08 -0700

    Something sort of working?

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,6 @@
-import {
-  ButtonItem,
-  definePlugin,
-  DialogButton,
-  Menu,
-  MenuItem,
-  PanelSection,
-  PanelSectionRow,
-  Router,
-  ServerAPI,
-  showContextMenu,
-  staticClasses,
-} from "decky-frontend-lib";
+import { Toggle } from "decky-frontend-lib";
 import { ReactElement, VFC } from "react";
 import { SMM } from "@crankshaft/types";
-import { FaShip } from "react-icons/fa";
-
-import logo from "../assets/logo.png";
 
 // interface AddMethodArgs {
 //   left: number;
@@ -26,7 +11,12 @@ export const load = (smm: SMM) => {
   console.log("Decky plugin loaded");
 
   const render = async (smm: SMM): Promise<ReactElement> => {
-    return <div>Hello world!</div>;
+    return (
+      <div id="decky-root">
+        <div>Hello world!</div>
+        <Toggle value={false} />
+      </div>
+    );
   };
 
   smm.MenuManager.addMenuItem({
@@ -34,6 +24,7 @@ export const load = (smm: SMM) => {
     label: "Decky",
     fontSize: 16,
     render: async (smm: SMM, root: HTMLElement) => {
+      //@ts-ignore
       SP_REACTDOM.render(await render(smm), root);
     },
   });
@@ -42,87 +33,4 @@ export const load = (smm: SMM) => {
 export const unload = (smm: SMM) => {
   console.info("Decky plugin unloaded!");
   smm.MenuManager.removeMenuItem("decky-plugin");
-};
-
-const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
-  // const [result, setResult] = useState<number | undefined>();
-
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
-
-  return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
-        >
-          Server says yolo
-        </ButtonItem>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Router.CloseSideMenus();
-            Router.Navigate("/decky-plugin-test");
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>
-    </PanelSection>
-  );
-};
-
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Router.NavigateToStore()}>
-        Go to Store
-      </DialogButton>
-    </div>
-  );
-};
-
-const defineThisPlugin = (serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
-
-  return {
-    title: <div className={staticClasses.Title}>Example Plugin</div>,
-    content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
-    onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
-    },
-  };
 };
